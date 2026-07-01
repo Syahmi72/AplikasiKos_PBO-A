52c92b0499e273d0646056ec3c03f74a0546d7b8
Syahmi <04221052@stundent.itk.ac.id>
2026-07-01 08:19:28 +0800

    Commit Kedua: Menambahkan class KamarKos dan fungsi cariKamar

--- AplikasiKos.kt
+++ AplikasiKos.kt
@@ -4,3 +4,11 @@ open class User(val userId: String, val nama: String, val email: String) {
 
 class Pencarikos(userId: String, nama: String, email: String): User(userId, nama, email)
 class Pemilikkos(userId: String, nama: String, email: String, val rekening: String): User(userId, nama, email)
+
+class Kamarkos(val nomorkamar: String, val harga: Int) {
+    var isTersedia: Boolean = true
+    fun ubahStatus() { isTersedia = !isTersedia }
+}
+
+// Menambahkan fungsi pencarian kamar untuk Pencarikos
+fun Pencarikos.cariKamar(list: List<Kamarkos>) = list.filter { it.isTersedia }
